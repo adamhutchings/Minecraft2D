9538779ce34ff14bcc4699763fce97a764d22eda
adamhutchings <adam.abahot@gmail.com>
2020-06-02 22:04:29 -0700

    Add recursion in file searching

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,5 @@ mkdir -p bin
 sfml_path=/usr/local/Cellar/sfml/2.5.1
 
 # Compile
-g++ src/*.cpp -I$sfml_path/include -L$sfml_path/lib -lsfml-graphics -lsfml-window -lsfml-system -o bin/mc2d
+g++ -g $(find src -type f -iregex ".*\.cpp") -std=c++11 \
+-I$sfml_path/include -L$sfml_path/lib -lsfml-graphics -lsfml-window -lsfml-system -o bin/mc2d
