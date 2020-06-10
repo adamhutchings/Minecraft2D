3552eb53feb9fe89536269dd1d3da7f1b67ad088
adamhutchings <adam.abahot@gmail.com>
2020-06-09 17:49:58 -0700

    Add player files

---
+++ src/entity/player.cpp
@@ -0,0 +1,35 @@
+#include "player.h"
+
+Player::Player(MWindow& window, double x, double y)
+: Entity(window, x, y) {
+
+	// Load the textures.
+	sf::Texture headTexLeft;
+	sf::Texture headTexRight;
+	sf::Texture bodyTex;
+	if (!headTexLeft.loadFromFile("res/head_left.jpg" ) ||
+		!headTexRight.loadFromFile("res/head_right.jpg") ||
+		!bodyTex.loadFromFile("res/body.jpg")) {
+		std::cout << "Player textures failed to load.\n";
+	}
+	headLeft.setTexture(headTexLeft, true);
+	headRight.setTexture(headTexRight, true);
+	body.setTexture(bodyTex, true);
+
+	// Facing right by default
+	this->facing = true;
+}
+
+void Player::render(MWindow& window) const {
+	window.draw(facing ? headRight : headLeft); // Brilliance in a nutshell
+	window.draw(body);
+}
+
+void Player::updateSpritePosition(MWindow& relativeTo) {
+	int xPos = x * 200 + 100 - relativeTo.xShift;
+	int yPos = -y * 200 + 100 - relativeTo.yShift;
+
+	headLeft.setPosition(xPos, yPos - 160);
+	headRight.setPosition(xPos, yPos - 160);
+	body.setPosition(xPos, yPos - 70);
+}
---
+++ src/entity/player.h
@@ -0,0 +1,24 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "entity.h"
+#include "../render/m_window.h"
+
+// Represents a player in the game.
+class Player : public Entity {
+private:
+	sf::Sprite headLeft; sf::Sprite headRight; // both 40x40
+	sf::Sprite body; // 23x140 (just the size I downloaded, may change)
+public:
+
+	// Is the player facing left (false) or right (true)?
+	bool facing;
+
+	Player(MWindow& window, double x, double y);
+	void render(MWindow& window) const override;
+	void updateSpritePosition(MWindow& relativeTo) override;
+};
+
+#endif // PLAYER_H
