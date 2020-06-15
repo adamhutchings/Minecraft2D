608fb38f369c4a24eeff49d45f1963f9e2d4734c
adamhutchings <adam.abahot@gmail.com>
2020-06-14 18:20:48 -0700

    Add world member and update constructor

--- src/entity/entity.cpp
+++ src/entity/entity.cpp
@@ -1,9 +1,17 @@
 #include "entity.h"
+#include "../render/renderable.h"
+#include "../world/world.h"
 
 // There will DEFINITELY be more code here later
 
-Entity::Entity(MWindow& window, double x, double y)
-: Renderable(window) {
+Entity::Entity(M2DWorld* o_world, double x, double y)
+: Renderable(*(o_world->i_window)) {
 	this->x = x;
 	this->y = y;
+	world = o_world;
+}
+
+bool Entity::isCollided() {
+	// Because worlds are simple right now, this doesn't need to be much.
+	return y < 10;
 }
--- src/entity/entity.h
+++ src/entity/entity.h
@@ -5,21 +5,30 @@
 #include "../render/m_window.h"
 #include "../render/renderable.h"
 
+class M2DWorld;
+
 // Base class for all entities
 
 class Entity : public Renderable {
 public:
+	M2DWorld* world;
+
 	int health;
 	int maxHealth; // The maximum it can go up to - spawn health below
+
+	int height;
 
 	// Entity coordinates need to be more precise!
 	double x;
 	double y;
 
-	sf::Vector2f velocity; // x and y components
+	double dx;
+	double dy;
 
-	Entity(MWindow& window, double x, double y);
+	Entity(M2DWorld* world, double x, double y);
 	virtual int getSpawnHealth() = 0;
+
+	bool isCollided();
 
 };
 
