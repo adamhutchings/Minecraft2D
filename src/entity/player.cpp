1957e735440593ac636e7f8868ca8516d285b8d5
adamhutchings <adam.abahot@gmail.com>
2020-06-18 16:17:14 -0700

    Add head rotation

--- src/entity/player.cpp
+++ src/entity/player.cpp
@@ -13,6 +13,11 @@ Player::Player(M2DWorld& world)
 	headRight.setTexture(HEAD_TEX_RIGHT, true);
 	body.setTexture(BODY_TEX, true);
 
+	// So rotations work more smoothly, rotation is relative to bottom middle
+	sf::Vector2f origin(40, 80);
+	headLeft.setOrigin(origin);
+	headRight.setOrigin(origin);
+
 	// Facing right by default
 	this->facing = true;
 }
@@ -26,8 +31,8 @@ void Player::updateSpritePosition(MWindow& relativeTo) {
 	int xPos = x * 200 + 100 - relativeTo.xShift;
 	int yPos = -y * 200 + 100 - relativeTo.yShift;
 
-	headLeft.setPosition(xPos - 35, yPos - 160);
-	headRight.setPosition(xPos, yPos - 160);
+	headLeft.setPosition(xPos + 5, yPos - 80);
+	headRight.setPosition(xPos + 40, yPos - 80);
 	body.setPosition(xPos, yPos - 80);
 }
 
@@ -39,6 +44,30 @@ void Player::setCenter() {
 	// Set the window to be centered on the player
 	world->i_window->xShift =  x*200 + 100 - WN_WIDTH / 2;
 	world->i_window->yShift = -y*200 + 100 - WN_HEIGHT / 2;
+}
+
+void Player::update(){
+	Entity::update();
+
+	/* Update head location */
+
+	sf::Vector2i pos = sf::Mouse::getPosition();
+
+	double pi = 2 * acos(0.0);
+
+	float xDiff = pos.x - PLAYER_HEAD_X;
+	float yDiff = PLAYER_HEAD_Y - pos.y; // This one goes up and down normally now
+
+	double rotation = (xDiff == 0) ? (yDiff > 0 ? -pi/4 : pi/4) : atan(yDiff / xDiff);
+
+	// Right head only faces right (-pi/2 to pi/2), left head left(-pi/2 to -pi/4, pi/4 to pi/2)
+	if (xDiff < 0) {
+		// Mouse is left
+		headLeft.setRotation(- rotation * 360 / (2 * pi));
+	} else {
+		headRight.setRotation(- rotation * 360 / (2 * pi));
+	}
+
 }
 
 sf::Vector2f& findSpawnLocation(M2DWorld& world) {
