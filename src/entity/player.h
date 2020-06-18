788584210da992a87e88e5cc2ca3c4ad20f715aa
adamhutchings <adam.abahot@gmail.com>
2020-06-18 16:16:48 -0700

    Correct outdated comments

--- src/entity/player.h
+++ src/entity/player.h
@@ -1,11 +1,16 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <cmath>
 #include <SFML/Graphics.hpp>
 #include "entity.h"
 #include "../render/m_window.h"
 #include "../main.h"
 #include "../item/inventory.h"
+
+// Hardcoded player head coords for now
+#define PLAYER_HEAD_X 1482
+#define PLAYER_HEAD_Y 2772
 
 inline sf::Texture HEAD_TEX_LEFT;
 inline sf::Texture HEAD_TEX_RIGHT;
@@ -18,8 +23,8 @@ sf::Vector2f& findSpawnLocation(M2DWorld& world);
 // Represents a player in the game.
 class Player : public Entity {
 private:
-	sf::Sprite headLeft; sf::Sprite headRight; // both 40x40
-	sf::Sprite body; // 23x140 (just the size I downloaded, may change)
+	sf::Sprite headLeft; sf::Sprite headRight; // both 80x80
+	sf::Sprite body; // 46x280 (just the size I downloaded, may change)
 public:
 
 	PlayerInventory* inventory;
@@ -32,6 +37,7 @@ public:
 	void updateSpritePosition(MWindow& relativeTo) override;
 	int getSpawnHealth() override;
 	void setCenter();
+	void update() override;
 };
 
 #endif // PLAYER_H
