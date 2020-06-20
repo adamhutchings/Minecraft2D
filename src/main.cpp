daad246759038a15f202cc46d3593623dd4d8600
adamhutchings <adam.abahot@gmail.com>
2020-06-20 16:46:09 -0700

    Add button font loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "main.h"
 
+void loadButtonFont() {
+	if (!M_BUTTON_FONT.loadFromFile("/Library/Fonts/arial.ttf")) {
+		std::cout << "Whoops! An error occurred.\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
 void loadTextures() {
 	if (!GRASS_TEX.loadFromFile("res/tex/grass.jpg") ||
 		!DIRT_TEX.loadFromFile("res/tex/dirt.jpg") ||
@@ -9,6 +16,7 @@ void loadTextures() {
 		!HEAD_TEX_RIGHT.loadFromFile("res/tex/head_right.jpg") ||
 		!BODY_TEX.loadFromFile("res/tex/body.jpg")) {
 		std::cout << "Whoops! Unable to load the texture.\n";
+		exit(EXIT_FAILURE);
 	}
 }
 
@@ -16,6 +24,8 @@ int main() {
 
 	// Setup
 	loadTextures();
+
+	loadButtonFont();
 	
 	// For now, just create a new window
 	MWindow wn(WN_WIDTH, WN_HEIGHT, "2D Minecraft");
