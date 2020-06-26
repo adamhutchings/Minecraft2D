d42b3a2892efff710b1e5353e5883836068999b2
adamhutchings <adam.abahot@gmail.com>
2020-06-26 16:32:14 -0700

    Add script to make necessary fodlers

---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+mkdir -p ./bin
+mkdir -p ./bin/objects
+mkdir -p ./bin/objects/entity
+mkdir -p ./bin/objects/world
+mkdir -p ./bin/objects/item
+mkdir -p ./bin/objects/render
+mkdir -p ./bin/objects/gui
