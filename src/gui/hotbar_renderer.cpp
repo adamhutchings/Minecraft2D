e96076a40b9ccf5451edc17c399d615a6b819794
adamhutchings <adam.abahot@gmail.com>
2020-07-10 23:45:20 -0700

    Add frame for hotbar rendering

--- src/gui/hotbar_renderer.cpp
+++ src/gui/hotbar_renderer.cpp
@@ -1,1 +1,28 @@
 #include "hotbar_renderer.h"
+
+InventoryFrame::InventoryFrame(MWindow& window, int x, int y)
+: Renderable(window),
+// Initialize the rects
+b(sf::Vector2f(FRAME_SIZE, FRAME_THICKNESS)),
+t(sf::Vector2f(FRAME_SIZE, FRAME_THICKNESS)),
+l(sf::Vector2f(FRAME_THICKNESS, FRAME_SIZE)),
+r(sf::Vector2f(FRAME_THICKNESS, FRAME_SIZE)) {
+	// Giving positions to the various elements
+	b.setPosition(sf::Vector2f(x - (FRAME_SIZE / 2), y + (FRAME_SIZE / 2) - FRAME_THICKNESS));
+	t.setPosition(sf::Vector2f(x - (FRAME_SIZE / 2), y - (FRAME_SIZE / 2)));
+	l.setPosition(sf::Vector2f(x - (FRAME_SIZE / 2), y - (FRAME_SIZE / 2)));
+	r.setPosition(sf::Vector2f(x + (FRAME_SIZE / 2) - (FRAME_THICKNESS / 2), y - FRAME_SIZE / 2));
+}
+
+void InventoryFrame::render(MWindow& window) const {
+	// Just draw all the rectangles
+	window.draw(b);
+	window.draw(t);
+	window.draw(l);
+	window.draw(r);
+
+}
+
+void renderHotbar(MWindow& window, InventorySection* section) {
+
+}
--- src/gui/hotbar_renderer.h
+++ src/gui/hotbar_renderer.h
@@ -1,6 +1,25 @@
 #ifndef M_HB_RENDERER_H
 #define M_HB_RENDERER_H
 
+#include <SFML/Graphics.hpp>
+#include "../render/m_window.h"
+#include "../item/inventory.h"
+#include "../render/renderable.h"
 
+#define FRAME_THICKNESS 20
+#define FRAME_SIZE 160
+
+inline sf::Font INVENTORY_FONT;
+
+class InventoryFrame : public Renderable {
+public:
+	sf::RectangleShape b, t, l, r; // The four sides
+	InventoryFrame(MWindow& window, int xPos, int yPos);
+	void render(MWindow& window) const override;
+};
+
+// This provides a method to render an inventory section in a straight line
+// at the bottom of the screen.
+void renderHotbar(MWindow& window, InventorySection* section);
 
 #endif // M_HB_RENDERER_H
