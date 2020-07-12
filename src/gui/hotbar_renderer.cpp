b22c9427237b9604373049ddeaa8d676e8b55f08
adamhutchings <adam.abahot@gmail.com>
2020-07-12 16:55:18 -0700

    Add complete rendering (no # yet!)

--- src/gui/hotbar_renderer.cpp
+++ src/gui/hotbar_renderer.cpp
@@ -1,7 +1,7 @@
 #include "hotbar_renderer.h"
 
 InventoryFrame::InventoryFrame(MWindow& window, int x, int y)
-: Renderable(window),
+:
 // Initialize the rects
 b(sf::Vector2f(FRAME_SIZE, FRAME_THICKNESS)),
 t(sf::Vector2f(FRAME_SIZE, FRAME_THICKNESS)),
@@ -14,15 +14,24 @@ r(sf::Vector2f(FRAME_THICKNESS, FRAME_SIZE)) {
 	r.setPosition(sf::Vector2f(x + (FRAME_SIZE / 2) - (FRAME_THICKNESS / 2), y - FRAME_SIZE / 2));
 }
 
-void InventoryFrame::render(MWindow& window) const {
+void InventoryFrame::render(MWindow& window) {
 	// Just draw all the rectangles
 	window.draw(b);
 	window.draw(t);
 	window.draw(l);
 	window.draw(r);
-
 }
 
 void renderHotbar(MWindow& window, InventorySection* section) {
-
+	int centerX;
+	for (int i = 0; i < section->size; i++) {
+		centerX = WN_WIDTH / 2 - (section->size * FRAME_SIZE / 2) + (i * FRAME_SIZE);
+		InventoryFrame(window, centerX, HOTBAR_DISPLAY_HEIGHT).render(window);
+		section->contents[i]->spr.setPosition(
+			centerX - (200 * 0.4 / 2), HOTBAR_DISPLAY_HEIGHT - (200 * 0.4 / 2)
+		);
+		if (section->contents[i]->name != "empty") {
+			window.draw(section->contents[i]->spr);	
+		}
+	}
 }
